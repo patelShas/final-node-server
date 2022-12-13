de64561b6682fbb5766db74d18f5504953e8bff0
patelShas <patel.shas@northeastern.edu>
2022-12-13 17:11:53 -0500

    works remote now

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ import UsersController
     from "./controllers/users/users-controller.js";
 import ReviewsController from "./controllers/reviews/reviews-controller.js";
 import mongoose from "mongoose";
-mongoose.connect('mongodb://127.0.0.1:27017/spotty_reviews');
+const CONNECTION_STRING = "mongodb+srv://spotty_reviews:sraccess@cluster0.sr1nuyy.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
