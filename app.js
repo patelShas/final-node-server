04470e7401f3abe996e0cb37825800256891eb5a
patelShas <patel.shas@northeastern.edu>
2022-12-14 20:25:08 -0500

    user session stuff

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 import express from 'express';
 import cors from 'cors'
-import HelloController
-    from "./controllers/hello-controller.js";
 import UsersController
-    from "./controllers/users/users-controller.js";
+    , {anon} from "./controllers/users/users-controller.js";
 import ReviewsController from "./controllers/reviews/reviews-controller.js";
+import SessionController from "./session/session-controller.js";
 import mongoose from "mongoose";
+
 const CONNECTION_STRING = "mongodb+srv://spotty_reviews:sraccess@cluster0.sr1nuyy.mongodb.net/?retryWrites=true&w=majority"
+import session from 'express-session'
+
 mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
-HelloController(app);
+app.set('trust proxy', 1)
+app.use(session({
+    secret: "smthn_lol",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false},
+}))
 UsersController(app);
 ReviewsController(app);
+SessionController(app);
 app.listen(4000);
--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -3,8 +3,19 @@ import * as usersDao from './users-dao.js'
 
 const UsersController = (app) => {
     app.get('/api/users', findUsers)
-    app.post('/api/register', register);
-    app.post('/api/login', login);
+    app.get('/api/auth/profile', profile)
+    app.post('/api/auth/register', register);
+    app.post('/api/auth/login', login);
+    app.post('/api/auth/logout', logout);
+
+}
+
+export const anon = {
+    username: "Anon",
+    password: "N/A",
+    type: "ANON",
+    bio: "You aren't currently logged in. Some content is unavailable",
+    following: []
 }
 
 const findUsers = async (req, res) => {
@@ -12,12 +23,27 @@ const findUsers = async (req, res) => {
     res.json(users)
 }
 
+const profile = (req, res) => {
+    res.send(req.session['profile'] || anon);
+}
+
 const login = async (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
     const user = await usersDao.findUserByCredentials(username, password)
-    res.send(user || null)
+    if (user) {
+        req.session['profile'] = user;
+        res.send(user)
+    } else {
+        res.send(anon)
+    }
 }
+
+const logout = (req, res) => {
+    req.session.destroy();
+    res.send(anon);
+}
+
 
 const register = async (req, res) => {
     var username = req.body.username;
@@ -28,11 +54,12 @@ const register = async (req, res) => {
         username: username, password: password, type: type, bio: bio, following: []
     };
     var presentUser = await usersDao.findUser(username)
-    if (!presentUser) {
+    if (!presentUser && username !== "Anon") {
         await usersDao.createUser(newUser)
+        req.session['profile'] = newUser;
         res.send(newUser)
     } else {
-        res.send(null)
+        res.send(anon)
     }
 }
 
